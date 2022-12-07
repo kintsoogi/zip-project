7b835ea477a81fed23c36ac254436ebc6bb4bc6a
kintsoogi <kintsoogii@proton.me>
2022-12-07 16:09:15 -0600

    Created component to display interface for creating a zip project and moved it out of App.js

--- src/App.js
+++ src/App.js
@@ -1,80 +1,11 @@
 import React, { useState } from "react";
 
-import ZipFileInput from "./components/ZipFileInput";
-import useZipToUsfmData from "./hooks/useZipToUsfmData";
-import useLocalForage from "./hooks/useLocalForage";
+import OpenZipProject from "./components/OpenZipProject";
 
 const App = () => {
-  const [projectName, setProjectName] = useState("");
-  const { store, getFromStore, setInStore } = useLocalForage("zip-store");
-
-  // On app load, check zip store for any existing projects...
-  // getAllFromStore
-
-  // If any projects exist and user wants to open, get info from store
-  // getFromStore(project)
-
-  const handleZipLoad = async (file) => {
-    const arrayBuffer = await file.arrayBuffer();
-    await setInStore(projectName, arrayBuffer);
-  };
-
-  const {
-    status,
-    file,
-    isLoading,
-    usfmData,
-    invalidFileType,
-    uploadError,
-    onChange,
-    onSubmit,
-  } = useZipToUsfmData(handleZipLoad);
-
-  if (isLoading) {
-    return <div>Loading....</div>;
-  }
-
-  if (uploadError) {
-    return (
-      <div>
-        <h1>An Error occured:</h1>
-        <p>{uploadError.message}</p>
-      </div>
-    );
-  }
-
-  if (invalidFileType) {
-    return (
-      <div>
-        <p>{`Invalid file upload: ${invalidFileType}`}</p>
-      </div>
-    );
-  }
-
-  if (status === "UPLOAD_SUCCESS") {
-    console.log(usfmData);
-    return (
-      <div>
-        <p>wohoo it worked!</p>
-      </div>
-    );
-  }
-
   return (
     <div className="App">
-      <h1>This is my file input</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          placeholder="Project Name"
-          name="projectName"
-          value={projectName}
-          onChange={(e) => {
-            setProjectName(e.target.value);
-          }}
-        />
-        <ZipFileInput onChange={onChange} />
-        <button>Submit</button>
-      </form>
+      <OpenZipProject />
     </div>
   );
 };
--- src/components/OpenZipProject.cy.js
+++ src/components/OpenZipProject.cy.js
@@ -1,13 +1,12 @@
-import App from "./App";
-import ZipFileInput from "./components/ZipFileInput";
+import OpenZipProject from "./OpenZipProject";
 
-describe("<App>", () => {
+describe("<OpenZipProject>", () => {
   it("mounts", () => {
-    cy.mount(<App />);
+    cy.mount(<OpenZipProject />);
   });
 
   it("responds with an error when a user enters a file other than zip", () => {
-    cy.mount(<App />);
+    cy.mount(<OpenZipProject />);
     cy.get("[data-cy=zip-file-input]").selectFile(
       "cypress/fixtures/example.json"
     );
@@ -19,7 +18,7 @@ describe("<App>", () => {
   });
 
   it("responds with an error when a user enters a zip file without usfm files", () => {
-    cy.mount(<App />);
+    cy.mount(<OpenZipProject />);
     cy.get("[data-cy=zip-file-input]").selectFile("cypress/fixtures/lorem.zip");
     cy.get("button").click();
     cy.get("p").should(
@@ -29,7 +28,7 @@ describe("<App>", () => {
   });
 
   it("responds with an error when a user inputs a zip file with INVALID usfm files when validation is turned on", () => {
-    cy.mount(<App />);
+    cy.mount(<OpenZipProject />);
     cy.get("[data-cy=zip-file-input]").selectFile(
       "cypress/fixtures/invalid_usfm.zip"
     );
@@ -40,8 +39,8 @@ describe("<App>", () => {
     );
   });
 
-  it("fires an onChange event when a user uploads a valid zip file", () => {
-    cy.mount(<App />);
+  it("finishes properly if the user enters a valid zip file containing usfm files", () => {
+    cy.mount(<OpenZipProject />);
     cy.get("[data-cy=zip-file-input]").selectFile(
       "cypress/fixtures/Mat_Tit.zip"
     );
