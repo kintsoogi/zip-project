3f6518322a6cdaf63ae93b92e2f707e6e88fdff4
kintsoogi <kintsoogii@proton.me>
2022-12-23 14:42:51 -0600

    v0.0.14

--- src/utils/zipUsfmHelpers.js
+++ src/utils/zipUsfmHelpers.js
@@ -1,61 +1,65 @@
-import grammar from "usfm-grammar";
-import JSZip from "jszip";
+import grammar from 'usfm-grammar'
+import JSZip from 'jszip'
 
-const USFM_REGEX = /([a-zA-Z0-9\s_\\.\-():])+(.usfm)$/i;
-const BOOK_ID_REGEX = /^\\id ([A-Z0-9]{3})/;
+const USFM_REGEX = /([a-zA-Z0-9\s_\\.\-():])+(.usfm)$/i
+const BOOK_ID_REGEX = /^\\id ([A-Z0-9]{3})/
 
-const isValidUsfmFile = (usfmText) => {
-  const usfmParser = new grammar.USFMParser(usfmText, grammar.LEVEL.RELAXED);
-  return usfmParser.validate();
-};
+const isValidUsfmFile = usfmText => {
+  const usfmParser = new grammar.USFMParser(usfmText, grammar.LEVEL.RELAXED)
+  return usfmParser.validate()
+}
 
-export const validateUsfmData = (usfmData) => {
-  const usfmValidatedData = usfmData.filter((usfmObj) =>
+export const validateUsfmData = usfmData => {
+  const usfmValidatedData = usfmData.filter(usfmObj =>
     isValidUsfmFile(usfmObj.usfmText)
-  );
-  return usfmValidatedData;
-};
+  )
+  return usfmValidatedData
+}
 
-const getUsfmTexts = async (zip) => {
+const getUsfmTexts = async zip => {
   const usfmPromises = zip
     .file(USFM_REGEX)
     // Transforms zip file objects to promises to get file data
-    .map((zipObj) => zipObj.async("string"));
+    .map(zipObj => zipObj.async('string'))
 
-  return await Promise.all(usfmPromises);
-};
+  return await Promise.all(usfmPromises)
+}
 
-const extractBookIdFromText = (usfmText) => {
-  const found = usfmText.match(BOOK_ID_REGEX);
-  const textBookId = found[1];
-  return textBookId;
-};
+const extractBookIdFromText = usfmText => {
+  try {
+    const found = usfmText.match(BOOK_ID_REGEX)
+    const textBookId = found[1]
+    return textBookId
+  } catch (error) {
+    throw new Error('USFM Text Invalid! ~ Did not contain Book ID')
+  }
+}
 
-export const arrayBufferToUsfmData = async (zipArrayBuffer) => {
+export const arrayBufferToUsfmData = async zipArrayBuffer => {
   // Create jszip and load data from array buffer
-  const zip = await JSZip.loadAsync(zipArrayBuffer);
+  const zip = await JSZip.loadAsync(zipArrayBuffer)
 
-  const usfmTexts = await getUsfmTexts(zip);
+  const usfmTexts = await getUsfmTexts(zip)
 
   const usfmDataArray = zip.file(USFM_REGEX).map((zipObj, fileIndex) => {
     return {
       filename: zipObj.name,
       usfmText: usfmTexts[fileIndex],
       bookId: extractBookIdFromText(usfmTexts[fileIndex]),
-    };
-  });
+    }
+  })
 
-  return usfmDataArray;
-};
+  return usfmDataArray
+}
 
-export const usfmDataToFileData = async (usfmData) => {
-  const zip = new JSZip();
+export const usfmDataToFileData = async usfmData => {
+  const zip = new JSZip()
 
   usfmData.forEach(({ filename, usfmText }) => {
-    zip.file(filename, usfmText);
-  });
+    zip.file(filename, usfmText)
+  })
 
-  const blob = await zip.generateAsync({ type: "blob" });
-  const arrayBuffer = await blob.arrayBuffer();
-  return { blob, arrayBuffer };
-};
+  const blob = await zip.generateAsync({ type: 'blob' })
+  const arrayBuffer = await blob.arrayBuffer()
+  return { blob, arrayBuffer }
+}
